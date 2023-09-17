190d41058166e10bf7b9abe42c6e833268aeb7b7
Ahmed Khaled <77169294+AhmedKhaleda998@users.noreply.github.com>
2023-09-17 20:13:59 +0300

    Error message update

--- middleware/checkUser.js
+++ middleware/checkUser.js
@@ -1,13 +1,13 @@
 exports.isCustomer = (req, res, next) => {
     if (req.userRole !== 'customer') {
-        return res.status(403).json({ error: 'Not Authorized' });
+        return res.status(403).json({ error: 'Not Authorized. You are a customer' });
     }
     next();
 };
 
 exports.isAdmin = (req, res, next) => {
     if (req.userRole !== 'admin') {
-        return res.status(403).json({ error: 'Not Authorized' });
+        return res.status(403).json({ error: 'Not Authorized. You are an admin' });
     }
     next();
 }
--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     const authHeader = req.get('Authorization');
     if (!authHeader) {
-        return res.status(403).json({ error: 'Not Authorized' });
+        return res.status(403).json({ error: 'Not Authorized. Token not sent' });
     }
     const token = authHeader.split(' ')[1];
     let decodedToken;
     try {
         decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     } catch (error) {
-        return res.status(403).json({ error: 'Not Authorized' });
+        return res.status(403).json({ error: 'Not Authorized. Token has been manipulated' });
     }
     if (!decodedToken) {
         return res.status(401).json({ error: 'Authentication failed' });
