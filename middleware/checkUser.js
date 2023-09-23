83679df39dfffb5d7ea030dc8236d1dbcf51a5b2
Ahmed Khaled <77169294+AhmedKhaleda998@users.noreply.github.com>
2023-09-23 16:58:09 +0300

    Update response messages

--- middleware/checkUser.js
+++ middleware/checkUser.js
@@ -1,13 +1,13 @@
 exports.isCustomer = (req, res, next) => {
     if (req.userRole !== 'customer') {
-        return res.status(403).json({ error: 'Not Authorized. You are a customer' });
+        return res.status(403).json({ error: 'Not Authorized. You are not a customer' });
     }
     next();
 };
 
 exports.isAdmin = (req, res, next) => {
     if (req.userRole !== 'admin') {
-        return res.status(403).json({ error: 'Not Authorized. You are an admin' });
+        return res.status(403).json({ error: 'Not Authorized. You are not an admin' });
     }
     next();
 }
